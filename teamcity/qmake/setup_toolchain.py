874b69bfe6788cc0b365e6e92bd927f78804f0d3
Török Róbert <torok.robert@symboid.com>
2020-12-28 09:48:57 +0100

    Create setup_toolchain.py

---
+++ teamcity/qmake/setup_toolchain.py
@@ -0,0 +1,59 @@
+
+import sys
+import os
+
+if not len(sys.argv) == 2:
+    print("Number of arguments must be 1!")
+    print("    arg #1: toolchain")
+    os._exit(1)
+
+toolchain = sys.argv[1]
+
+#qtdir = r'' + os.environ['QTHOME'] + r''
+#if not os.path.exists(qtdir):
+#    print "QTDIR '" + qtdir + "' does not exist! Aborting..."
+#    os._exit(1)
+
+is_msvc_toolchain = (toolchain[:4] == "msvc")
+
+# make path
+if is_msvc_toolchain:
+    make_path = r'"' + os.environ['QTHOME'] + r'\\Tools\\QtCreator\\bin\\jom.exe"'
+else:
+    make_path = 'make'
+print("Path of make binary : "+make_path)
+
+# setting teamcity parameters:
+print "##teamcity[setParameter name='build.make_path' value='" + make_cmd + "']"
+
+'''
+
+# toolchain setup script:
+if toolchain == "msvc2013":
+    toolchain_script = r'"' + os.environ['VS120COMNTOOLS'] + r'\..\..\VC\vcvarsall.bat"'
+elif  toolchain == "msvc2015":
+    toolchain_script = r'"' + os.environ['VS140COMNTOOLS'] + r'\..\..\VC\vcvarsall.bat"'
+else:
+    toolchain_script = ""
+
+# qmake setup shell script call:
+if platform == "win_x86":
+    invoke_env_script = "CALL " + toolchain_script + " x86"
+elif platform == "win_x64":
+    invoke_env_script = "CALL " + toolchain_script + " x64"
+else:
+    invoke_env_script = "echo"
+
+# PATH setting shell script call:
+if platform == "win_x86" or platform == "win_x64":
+    invoke_set_path = r'set "PATH=' + qtdir + r'\\bin;' + os.environ['PATH'] + r'"'
+    make_cmd = r'"' + os.environ['QT_HOME'] + r'\\Tools\\QtCreator\\bin\\jom.exe"'
+else:
+    invoke_set_path = r'export PATH="' + qtdir + r'/bin:' + os.environ['PATH'] + r'"'
+    make_cmd = 'make'
+
+# setting teamcity parameters:
+print "##teamcity[setParameter name='build.make_path' value='" + make_cmd + "']"
+print "##teamcity[setParameter name='qmake_setup_env' value='" + invoke_env_script + "']"
+print "##teamcity[setParameter name='qmake_set_path' value='" + invoke_set_path + "']"
+'''
