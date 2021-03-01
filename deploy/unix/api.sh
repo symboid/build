18e9c739b1ee0ebab53d7d2d6d9bb32cfbcad8b7
Török Róbert <robert.torok@gmail.com>
2021-03-01 08:26:12 +0100

    API tar.gz created

--- deploy/unix/api.sh
+++ deploy/unix/api.sh
@@ -5,11 +5,19 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../.." && pwd)"
 function ComponentApiBegin
 {
     COMPONENT_NAME=$1
-    BUILD_DIR=$2
-    local ARCHIVE_FOLDER=$3
-    ARCHIVE_PATH=$ARCHIVE_FOLDER/$COMPONENT_NAME-api.tar
+    REL_BUILD_DIR=$2
+    local REL_ARCHIVE_FOLDER=$3
+    ARCHIVE_PATH=$ROOT_DIR/$REL_ARCHIVE_FOLDER/$COMPONENT_NAME-api.tar.gz
+    ARCHIVE_TMP_DIR=$ROOT_DIR/$REL_ARCHIVE_FOLDER/$COMPONENT_NAME-api
     echo "Creating $ARCHIVE_PATH..."
-    printf '' | tar -cf $ARCHIVE_PATH --files-from -
+    rm -rf $ARCHIVE_TMP_DIR
+    mkdir -p $ARCHIVE_TMP_DIR
+}
+
+function ComponentApiEnd
+{
+    cd $ARCHIVE_TMP_DIR
+    tar --create --gzip --file=$ARCHIVE_PATH *
 }
 
 function FileApi
@@ -17,7 +25,8 @@ function FileApi
     local FILE_PATH=$1
     if [ -f $ROOT_DIR/$FILE_PATH ]; then
         echo "Adding file $FILE_PATH"
-        tar --append --file=$ARCHIVE_PATH  --directory=$ROOT_DIR $FILE_PATH
+        mkdir -p $(dirname $ARCHIVE_TMP_DIR/$FILE_PATH)
+        cp $ROOT_DIR/$FILE_PATH $ARCHIVE_TMP_DIR/$FILE_PATH
     else
         echo "File '$FILE_PATH' cannot be found!"
     fi
@@ -25,19 +34,20 @@ function FileApi
 
 function FolderApi
 {
-    local FOLDER_PATH=$1
+    local REL_FOLDER_PATH=$1
     local FILE_FILTER=$2
-    if [ -d $ROOT_DIR/$FOLDER_PATH ]; then
-        echo "Adding files $FILE_FILTER folder $FOLDER_PATH"
-        tar --append --file=$ARCHIVE_PATH --directory=$ROOT_DIR $FOLDER_PATH
+    if [ -d $ROOT_DIR/$REL_FOLDER_PATH ]; then
+        echo "Adding files $FILE_FILTER folder in $REL_FOLDER_PATH"
+        mkdir -p $ARCHIVE_TMP_DIR/$REL_FOLDER_PATH
+        cp -r $ROOT_DIR/$REL_FOLDER_PATH/$FILE_FILTER $ARCHIVE_TMP_DIR/$REL_FOLDER_PATH
     else
-        echo "Folder '$FOLDER_PATH' cannot be found!"
+        echo "Folder '$ROOT_DIR/$REL_FOLDER_PATH' cannot be found!"
     fi
 }
 
 function ModuleApi
 {
     local MODULE_NAME=$1
-    FolderApi $COMPONENT_NAME/$MODULE_NAME *.h
-#    FolderApi $BUILD_DIR/$COMPONENT_NAME/$MODULE_NAME *.dylib
+    FolderApi $COMPONENT_NAME/$MODULE_NAME '*.h'
+    FolderApi $REL_BUILD_DIR/$COMPONENT_NAME/$MODULE_NAME '*.so'
 }
