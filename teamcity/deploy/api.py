27958649954202ec3ec8d19a4825c3a982f1c8cf
Török Róbert <torok.robert@symboid.com>
2021-03-11 15:00:52 +0100

    Update api.py

--- teamcity/deploy/api.py
+++ teamcity/deploy/api.py
@@ -5,13 +5,11 @@ from subprocess import call
 
 if not len(sys.argv) == 5:
     print("Number of arguments must 3!")
-    print("    arg #1: component script path")
-    print("    arg #2: build dir")
-    print("    arg #3: package dir")
-    print("    arg #4: toolchain")
+    print("    arg #1: build dir")
+    print("    arg #2: package dir")
+    print("    arg #3: toolchain")
     os._exit(1)
 
-script_path = sys.argv[1]
 build_dir = sys.argv[2]
 package_dir = sys.argv[3]
 toolchain = sys.argv[4]
@@ -19,16 +17,17 @@ toolchain = sys.argv[4]
 # Build host is Windows: NSIS packaging
 if toolchain[:4] == "msvc":
     cmd = "makensis"
-    script = script_path + ".nsi"
+    script = "api.nsi"
     script_arguments = [ "/D_Config_BuildDir="+build_dir, "/D_Config_PackageDir="+package_dir, "/D_Config_Toolchain="+toolchain[:8] ]
     if not toolchain[-3:] == "_64":
         script_arguments.append("/D_Config_x86")
     process_arguments = [cmd] + script_arguments + [script]
-# Build host is other (Linux,MacOS): tar+gzip via api.sh
+# Build host is other (Linux,MacOS): tar+gzip
 else:
     cmd = "/bin/bash"
-    script = script_path + ".sh"
-    script_arguments = [ build_dir, package_dir ]
+    script = "tar"
+    os.chdir(build_dir)
+    script_arguments = [ "--create", "--gzip", "--file="+package_dir+"/api.tar.gz" ]
     process_arguments = [cmd] + [script] + script_arguments
 
 print("Script interpreter : " + cmd)
